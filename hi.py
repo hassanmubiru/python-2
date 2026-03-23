03ae8806d876e77afe8a4f0b865e854f5b69d49f
hassanmubiru <hassanmubiru7@gmail.com>
2026-03-23 22:53:18 +0300

    new file:   hi.py

---
+++ hi.py
@@ -0,0 +1,67 @@
+# print('Hello world')
+# print("Welcome to Python")
+# print(42)
+# # print(3.14)
+
+#variables
+
+# name = "David"
+# age = 30
+# score = 90.5
+
+# print(name)
+# print(age)
+
+# #updating
+# age = 29
+# score += 80
+# # print(score)
+
+#
+# a = 'cat'
+# b = 'dog'
+# a,b = b,a
+# print(a,b)
+
+# x =y=z = 'banana'
+# print(x)
+# print(y)
+# print(z)
+
+# Dataypes
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
+
+# name = "Hello"
+# print(name)
+
+# age_str = '30'
+# age_int = int(age_str)
+# print(age_int + 5)
+
+# #complex
+# x = 2j
+# print(x)
+# print(type(x))
+
+# #basic math
+# print(9 + 5) # addition
+# print(9-5) #subtraction
+# print(9*5) #multiplication
+# print(9// 5) # drop reminder
+# print(10/2)#division
+# print(9 % 5) # modulo
+# print(9**5) # power
+
+# user input
+name = input("What is your name")
+print("Hello" + name + "!")
+
+age = int(input("How Old are you"))
+print("Next year you will be ",age)
