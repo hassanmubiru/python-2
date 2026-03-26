623c997a784bbed61d14f5f958e4de4ae9ccebcc
hassanmubiru <hassanmubiru7@gmail.com>
2026-03-26 22:56:07 +0300

    new file:   dic.py

---
+++ dic.py
@@ -0,0 +1,13 @@
+thisdict ={
+    "brand" : "Ford",
+    "model" : "Mustang",
+    "year" : 1964,
+    # "colors" : ["red","blue","green"]
+
+}
+# print(type(thisdict))
+# print(len(thisdict))
+# thisdict.update({'colors':'yellow'})
+# thisdict.pop("brand")
+# thisdict.popitem()
+# print(thisdict)
