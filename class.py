c09d04b5b5588730ee380d7fcdc8a6a82e0f6e34
hassanmubiru <hassanmubiru7@gmail.com>
2026-04-10 12:04:32 +0300

    modified:   class.py

--- class.py
+++ class.py
@@ -9,8 +9,9 @@ class Parrot:
     name = ""
     age = 0
 
+
     # create parrot1 as object
-    parrot1 = Parrot()
+    parrot1  = Parrot()
     parrot1.name = "blue"
     parrot1.age = 10
 
@@ -21,4 +22,4 @@ class Parrot:
 
     # access attributes
     print(f"{parrot1.name} is {parrot1.age} year old")
-    print(f"
+    print(f"{parrot2.name} is {parrot2.age} year old")
