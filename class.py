b52cd465243a87668b737c1932b230addaee0b80
hassanmubiru <hassanmubiru7@gmail.com>
2026-04-10 11:48:00 +0300

    modified:   class.py

--- class.py
+++ class.py
@@ -1,5 +1,10 @@
-class MyClass:
-    x = 5
-result = MyClass()
-print(result.x)
+# class MyClass:
+#     x = 5
+# result = MyClass()
+# print(result.x)
 
+
+class Parrot:
+    # class attribute
+    name = ""
+    age = 0
