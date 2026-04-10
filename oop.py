f813da949006e594c2b8548cabb575862272663f
hassanmubiru <hassanmubiru7@gmail.com>
2026-04-10 12:22:24 +0300

    Changes to be committed: 	deleted:    class.py 	new file:   oop.py 	new file:   oop2.py

---
+++ oop.py
@@ -0,0 +1,18 @@
+class Parrot:
+    # class attributes
+    name = ""
+    age = 0
+
+# create parrot1 as object
+parrot1 = Parrot()
+parrot1.name = "blue"
+parrot1.age = 10
+
+# create parrot2 as object
+parrot2 = Parrot()
+parrot2.name = "woo"
+parrot2.age = 15
+
+# access attributes
+print(f"{parrot1.name} is {parrot1.age} years old")
+print(f"{parrot2.name} is {parrot2.age} years old")
---
+++ oop2.py
@@ -0,0 +1,21 @@
+# class MyClass:
+#     x = 5
+# result = MyClass()
+# print(result.x)
+
+
+class Parrot:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+parrot1 = Parrot("blu",10)
+parrot2 = Parrot("woo",15)
+print(f"{parrot1.name} is {parrot1.age} years old")
+print(f"{parrot2.name} is {parrot2.age} years old")
+
+
+
+
+
+    
