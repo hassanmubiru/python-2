719cd731537662784238531ae64bf90bb92f4e02
hassanmubiru <hassanmubiru7@gmail.com>
2026-04-11 21:23:49 +0300

    modified:   oop3.py

--- oop3.py
+++ oop3.py
@@ -2,3 +2,7 @@ class Car:
     def __init__(self,brand,model,year,mileage=0):
         self.brand = brand
         self.model = model
+        self.year  = year
+        self.mileage = mileage
+
+        
