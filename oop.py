4dd063c1158771b001263ac8e487f7eb9555bcb4
hassanmubiru <hassanmubiru7@gmail.com>
2026-04-11 19:47:25 +0300

    modified:   oop.py

--- oop.py
+++ oop.py
@@ -1,18 +1,25 @@
-class Parrot:
-    # class attributes
-    name = ""
-    age = 0
+# class Parrot:
+#     # class attributes
+#     name = ""
+#     age = 0
+#     def Parrot():
 
-# create parrot1 as object
-parrot1 = Parrot()
-parrot1.name = "blue"
-parrot1.age = 10
+# # create parrot1 as object
 
-# create parrot2 as object
-parrot2 = Parrot()
-parrot2.name = "woo"
-parrot2.age = 15
+# parrot1 = Parrot()
+# parrot1.name = "blue"
+# parrot1.age = 10
 
-# access attributes
-print(f"{parrot1.name} is {parrot1.age} years old")
-print(f"{parrot2.name} is {parrot2.age} years old")
+# # create parrot2 as object
+# parrot2 = Parrot()
+# parrot2.name = "woo"
+# parrot2.age = 15
+
+# # access attributes
+# print(f"{parrot1.name} is {parrot1.age} years old")
+# print(f"{parrot2.name} is {parrot2.age} years old")
+
+
+
+class Person:
+    name = "Denis"
