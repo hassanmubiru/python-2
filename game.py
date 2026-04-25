e1118afbd3d22fe5fb1c9a85983436602f77e286
hassanmubiru <hassanmubiru7@gmail.com>
2026-04-25 23:26:21 +0300

    modified:   game.py

--- game.py
+++ game.py
@@ -39,5 +39,5 @@ def message(msg,color):
 
     # Food position
     foodx = round(random.randrange(0,width - snake_block) /10.0)*10.0
-    foody = round(random.randrange(0,height - snake_block) /10.0))
+    foody = round(random.randrange(0,height - snake_block) /10.0)
 
