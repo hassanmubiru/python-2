700ef100188aefe25853596c92abfb040ad9a209
hassanmubiru <hassanmubiru7@gmail.com>
2026-04-25 23:22:04 +0300

    modified:   game.py

--- game.py
+++ game.py
@@ -38,5 +38,5 @@ def message(msg,color):
     Length_of_snake = 1
 
     # Food position
-    foodx = round(random.randrange(0,width - snake_block) /)
+    foodx = round(random.randrange(0,width - snake_block) /10.0)
 
