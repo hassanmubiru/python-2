0205b190d6ca2d387439af106cd522b5e8cf3882
hassanmubiru <hassanmubiru7@gmail.com>
2026-04-26 23:33:57 +0300

    modified:   game.py

--- game.py
+++ game.py
@@ -74,4 +74,4 @@ def message(msg,color):
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                game_over =
+                game_over = True
