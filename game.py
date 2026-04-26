2d30438ecad0f7d2c8e76b886b14c1f97feac42b
hassanmubiru <hassanmubiru7@gmail.com>
2026-04-26 23:26:31 +0300

    modified:   game.py

--- game.py
+++ game.py
@@ -60,3 +60,14 @@ def message(msg,color):
                 game_over = True
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT:
+                        x1_change = -snake_block
+                        y1_change = 0
+                    elif event.key == pygame.K_RIGHT:
+                        x1_change = snake_block
+                        y1_change = 0
+                    elif event.key == pygame.K_UP:
+                        y1_change = -snake_block
+                        x1_change = 0
+                    elif event.key == pygame.K_DOWN:
+                        y1_change = snake_block
+                        x1_change = 0
